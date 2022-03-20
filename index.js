f78b8276abb037c298f0f13383042e2b57e5650c
zuhi <ankita9shreya@gmail.com>
2022-03-20 09:02:18 +0530

    Adding ability to reduce the rating

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const star5 = document.querySelector('#star5');
 
 const ratingDiv = document.querySelector('#rating-result');
 
+function reset() {
+    const starDiv = document.querySelectorAll('.star img');
+    for(let i =0; i<starDiv.length;i++){
+        starDiv[i].src ='./empty-star.png';
+    }
+
+}
+
 function rating(starNo){
     const starDiv = document.querySelectorAll('.star img');
     for(let i =0; i<parseInt(starNo);i++){
@@ -15,19 +23,24 @@ function rating(starNo){
 }
 
 star1.addEventListener('click',(event)=>{
+    reset();
     rating("1");
 });
 
 star2.addEventListener('click',(event)=>{
+    reset();
     rating("2");
 });
 
 star3.addEventListener('click',(event)=>{
+    reset();
     rating("3");
 });
 star4.addEventListener('click',(event)=>{
+    reset()
     rating("4");
 });
 star5.addEventListener('click',(event)=>{
+    reset();
     rating("5");
 });
